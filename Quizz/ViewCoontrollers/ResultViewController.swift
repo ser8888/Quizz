40bdd5b234da6c1b78b36bda34e15d6328b4d1a0
ST <slokas85@gmail.com>
2022-07-10 08:18:48 +0300

    0818_10Jul

--- Quizz/ViewCoontrollers/ResultViewController.swift
+++ Quizz/ViewCoontrollers/ResultViewController.swift
@@ -12,7 +12,6 @@ class ResultViewController: UIViewController {
     
     private let questions = Question.getQuestions()
     var resultArray: [Answer] = []
-//    var resultArray: [String: Any] = [:]
     
     
     
@@ -24,35 +23,20 @@ class ResultViewController: UIViewController {
         print(resultArray)
  
     
-        
-//        dict = resultArray
-        
-        
-        
-//        resultArray.sort(on: { $0.animal }, by: < )
         print("S O R T E D    A R R A Y")
- //       print(newArray)
- //       let newArray = resultArray.sorted{ $0.animal.rawValue < $1.animal.rawValue }
-  //      print(newArray)
-//        print(newArray.first?.animal )
- //       var counts = [ String: Int ]()
- //       resultArray.forEach { counts[$0] = (counts[$0] ?? 0) + 1 }
+ 
+    
         
         
-        
-//        let indexAAA = resultArray.enumerated().map { (_, value. ) in }
+
         print(" T E S T")
-//        print(indexAAA)
+
         print(" T E S T")
             for index in 0..<resultArray.count {
              print(resultArray[index])
- //               var result[index] = String(resultArray[index])
+
        }
   
- //       for (key, value) in dictionary(resultArray[]) {
-            
-//        }
-     
         
     }
 
